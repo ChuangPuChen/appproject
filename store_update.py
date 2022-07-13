4ba4663779a13022a320228181bbb2a6e856984e
Chuang PuChen <107826332+ChuangPuChen@users.noreply.github.com>
2022-07-14 02:31:13 +0800

    Update store_update.py

--- store_update.py
+++ store_update.py
@@ -1,32 +1,35 @@
+from array import array
 from ast import copy_location
 import csv
-from xmlrpc.client import boolean
+from xmlrpc.client import Boolean, boolean
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from google.cloud.firestore import GeoPoint
 
 # 引用firebase私密金鑰
-# path/to/serviceAccount.json 請用自己存放的路徑
-cred = credentials.Certificate('servicekey.json')
+cred = credentials.Certificate('cat-demo1-firebase-adminsdk-r3ki7-c794738a4f.json')
 # 初始化firebase，注意不能重複初始化
 firebase_admin.initialize_app(cred)
 # 初始化firestore
 db = firestore.client()
 
-with open('ntc.csv', newline='') as csvfile:
+with open('store.csv', newline='') as csvfile:
 
   # 讀取 CSV 檔內容，將每一列轉成一個 dictionary
   rows = csv.DictReader(csvfile)
   # 以迴圈輸出指定欄位
   for row in rows:
     print(row)
-    op1=(row["op1"])
-    op2=(row["op2"])
-    op3=(row["op3"])
-    op4=(row["op4"])
-    op5=(row["op5"])
+    op1=(row["adoption"])
+    op2=(row["meal"])
+    op3=(row["donation"])    
     latitude=row['latitude']
     longitude=row['longitude']
-    doc_ref = db.collection("database").document("storelist")
-    doc_ref.update({row["name"]:{"lat":float(row["b"]),"log":float(row["a"]),"option1":boolean(op1=="1"),"option2":boolean(op2=="1"),"option3":boolean(op3=="1"),"option4":boolean(op4=="1"),"option5":boolean(op5=="1")}})
+    # 指定寫入資料的位置在'database'中的'store_list'
+    doc_ref = db.collection("database").document("store_list")
+    # 以update的形式將資料輸入document
+    doc_ref.update({row["name"]:{"location":firestore.GeoPoint(float(latitude),float(longitude)),
+    "store_info":{"weekday_text":str(row["weekday_text"]),"reserve_mode":str(row["reserve_mode"]),"spending_mode":str(row["spending_mode"])},    
+    "store_tag":{"adoption":(boolean(op1==1)),"meal":(boolean(op2==1)),"donation":(boolean(op3==1))},
+    "store_area":{"city":str(row["city"]),"district":str(row["district"]),"address":str(row["address"])}}})
