3a65b28a1ce7ba372ff4b00d7343a6ad9e9a6579
Chuang PuChen <107826332+ChuangPuChen@users.noreply.github.com>
2022-07-14 02:38:11 +0800

    Update mrt_update.py

--- mrt_update.py
+++ mrt_update.py
@@ -1,12 +1,12 @@
 import csv
+import string
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from google.cloud.firestore import GeoPoint
 
 # 引用firebase私密金鑰
-# path/to/serviceAccount.json 請用自己存放的路徑
-cred = credentials.Certificate('servicekey.json')
+cred = credentials.Certificate('cat-demo1-firebase-adminsdk-r3ki7-c794738a4f.json')
 # 初始化firebase，注意不能重複初始化
 firebase_admin.initialize_app(cred)
 # 初始化firestore
@@ -22,5 +22,5 @@ with open('mrtlocation_finish.csv', newline='') as csvfile:
     print(row)
     latitude=row['latitude']
     longitude=row['longitude']    
-    doc_ref = db.collection("database").document("mrtlist")
-    doc_ref.update({row["name"]:{"location":firestore.GeoPoint(float(latitude), float(longitude)),"tag1":str(row["tag1"]),"tag2":str(row["tag2"])}})
+    doc_ref = db.collection("database").document("mrt_list")
+    doc_ref.update({row["name"]:{"location":firestore.GeoPoint(float(latitude), float(longitude)),"station_tag":{"tag1":str(row["tag1"]),"tag2":str(row["tag2"])}}})
